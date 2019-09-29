23392519654bb23bd3f4e83710d33e41802f6b71
Aby <abhay.saxena003@gmail.com>
2019-09-29 21:57:33 +0530

    classes defined

--- simulator.py
+++ simulator.py
@@ -1,9 +1,38 @@
+from math import ceil
+
 tlb_algo = "FIFO"
 tlb = []
+tlb_size = 4
+ram_sz = 64
+hdd_sz = 256
+swap_sz = 128
+page_sz = 4
+proc_dict = {}
+
 ram = []
-swap = []
+
+class Page:
+
+
+
+class Entry:
+	# vpn = -1
+	# ppn = -1
+	# valid = False
+	# present = False
+	# pid = -1
+
+	def __init__(self, vp, pp):
+		self.vpn = vp
+		self.ppn = pp
+		self.valid = True
+		self.present = False
+
 class Process:
-	a = 0
+	def __init__(self, p, vsz, pti):
+		self.pid = p
+		self.vsize = vsz
+		self.ptable = [Entry(i,-1) for i in range(ceil(vsz/page_sz))]
 
 # Function to Display TLB, RAM, Swap Space
 def show_memory():
@@ -17,11 +46,50 @@ def insert_tlb(entry):
 			tlb.pop(0)
 			tlb.append(entry)
 		elif tlb_algo == "OPT":
+			a = 1
 			# To do
 		elif tlb_algo == "LRU":
+			a = 1
 			# To Do
 
-# Read Input
+def request_mem(pid, address):
+	if (proc_dict.has_key(pid)):
+		if (proc_dict[pid].pt_index == -1):
+			print("Bringing into memory")
 
+		else:
 
-# 
+	else:
+		print("Invalid process")
+
+# Initialize
+if __name__ == "__main__":
+	# Read Input
+	filename1 = "sample inputfile1.txt"
+	with open(filename1) as file_obj:
+		proc_lines = file_obj.read().split('\n')
+
+	for i in proc_lines:
+		k = i.split()
+		proc_dict[k[0]] = Process(k[0],k[1], -1)
+
+	filename2 = "sample inputfile2.txt"
+	with open(filename2) as file_obj:
+		access_lines = file_obj.read().strip().split('\n')
+
+	print(access_lines)
+
+	for i in range(len(access_lines)):
+		k = access_lines[i].split()
+		access_lines[i] = (int(k[0]),int(k[1]))
+
+	ram =[]
+
+	for i in access_lines:
+		print("Requesting memory for process %d and virtual address %d",i[0],i[1])
+		data = request_mem(i[0],i[1])
+		print(data)
+		print('\n')
+
+	# print(lines)
+	# 
